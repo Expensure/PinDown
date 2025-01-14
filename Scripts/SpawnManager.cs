dff01a57113faeaeccb0cb354e3e1ff42a1f76ab
Jasper van Loon <60733685+Expensure@users.noreply.github.com>
2025-01-14 11:23:40 +0000

    First Commit

---
+++ Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 8;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        SpawnPowerup();
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(spawnRange, -spawnRange);
+        float spawnPosZ = Random.Range(spawnRange, -spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0.3f, spawnPosZ);
+        return randomPos;
+    }
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+}
